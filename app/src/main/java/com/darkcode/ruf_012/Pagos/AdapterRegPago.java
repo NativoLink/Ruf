0867ddc503d9bc6e8aa113fc6caa8b4ca4e36832
luis rosario <ingluisrosario15@gmail.com>
2017-06-13 07:47:42 -0400

    Corrección de mi Project

---
+++ app/src/main/java/com/darkcode/ruf_012/Pagos/AdapterRegPago.java
@@ -0,0 +1,92 @@
+package com.darkcode.ruf_012.Pagos;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.darkcode.ruf_012.MainActivity;
+import com.darkcode.ruf_012.R;
+
+import java.util.List;
+
+/**
+ * Created by usuario on 12/05/2017.
+ */
+
+
+public class AdapterRegPago extends ArrayAdapter<ConsultaPendiente> {
+
+    private Context contexto;
+    private List<ConsultaPendiente> pago;
+
+    public AdapterRegPago(Context context, List<ConsultaPendiente> consultasPs) {
+        super(context, R.layout.list_consultas_pago,consultasPs);
+        contexto=context;
+        pago = consultasPs;
+
+
+    }
+    @Override
+    public View getView(final int position, View customView, ViewGroup parent) {
+        final ViewCPagoHolder holder;
+
+        int idps = pago.get(position).getId_consulta();
+        if (customView == null) {
+            holder = new ViewCPagoHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            customView = inflater.inflate(R.layout.list_consultas_pago, parent, false);
+
+            holder.idConsulta = (TextView) customView.findViewById(R.id.tvConsulta);
+            holder.pago_abono = (TextView) customView.findViewById(R.id.tvPago_o_abono);
+
+//            holder.btnSaldar = (Button) customView.findViewById(R.id.btnabonar);
+
+            customView.setTag(holder);
+        } else {
+            holder = (ViewCPagoHolder) customView.getTag();
+        }
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        //                  SE DEBE HABILITAR EL BOTON DE REMOVER
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//        holder.btnSaldar.setTag(position);
+
+//        holder.btnSaldar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+
+//            }
+//        });
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        //    EL MONTO A PAGAR O ABONAR DEBE SER ADD EN LA COMPROBACION DEL BOTTON ADD DE LA LISTA 1
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        int id = pago.get(position).getId_consulta();
+        String idC = Integer.toString(id);
+
+        int costo = pago.get(position).getCosto();
+        String costoP = Integer.toString(costo);
+
+        int pendienteP = pago.get(position).getPendiente();
+        String pen = Integer.toString(pendienteP);
+
+        holder.idConsulta.setText(idC);
+        holder.pago_abono.setText(pen);
+
+        return customView;
+
+    }
+
+    class ViewCPagoHolder {
+        TextView idConsulta;
+        TextView pago_abono;
+
+//        Button btnSaldar;
+
+    }
+}
